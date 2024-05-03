93ce3ebdf98593c3365454c6efa874830643226e
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-03 19:45:25 +0200

    feat: add  function

--- partials/functions.php
+++ partials/functions.php
@@ -5,16 +5,15 @@
         $password = [];
         $numOfCharacters = strlen($characters);
         for($i = 0; $i < $length; $i++){
+            $rndmCharacter = rand(0, $numOfCharacters);
             //se $allowRepetitions è vera fai girare la funzione normalmente
             if($bool){
-                $rndmCharacter = rand(0, $numOfCharacters);
                 $password[] = $characters[$rndmCharacter];
             //se $allowRepetitions è falsa genera un numero,
             //se il carattere è nell'array sparalo dentro 
             } else{
-                $rndmCharacter = rand(0, $numOfCharacters);
                 if(!in_array($characters[$rndmCharacter], $password)){
-                    $password[] = $characters[$rndmCharacter];
+                    $password[] = $characters[$rndmCharacter]; 
                 }
                 
             }
